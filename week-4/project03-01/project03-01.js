cb536d792946eb93ef99ce5e231cd3da1125e263
Chris Newell <84216346+ChrisNewelldev@users.noreply.github.com>
2025-04-19 07:00:50 -0700

    week 4 assignment complete

--- week-4/project03-01/project03-01.js
+++ week-4/project03-01/project03-01.js
@@ -1,20 +1,17 @@
-/*    JavaScript 7th Edition
-      Chapter 3
-      Project 03-01
-
-      Application to calculate total order cost
-      Author: Chris Newell
-      Date: 19/04/2025
-
-      Filename: project03-01.js
-*/
+// Filename: project03-01.js
+// Application to calculate total order cost
+// Author: Chris Newell
+// Date: 19/04/2025
 
 // Function to display a numeric value as a text string in the format $##.##
 function formatCurrency(value) {
   return '$' + value.toFixed(2);
 }
 
-// Get all the elements with the class "menuItem"
+// Declare menuItems BEFORE trying to use it
+let menuItems = document.getElementsByClassName('menuItem');
+
+// Add click event listeners to each checkbox
 for (let i = 0; i < menuItems.length; i++) {
   menuItems[i].addEventListener('click', calcTotal);
 }
@@ -23,19 +20,13 @@ for (let i = 0; i < menuItems.length; i++) {
 function calcTotal() {
   let orderTotal = 0;
 
-  // loop through all the menu items and add their values if checked
+  // Loop through checkboxes and sum checked values
   for (let i = 0; i < menuItems.length; i++) {
     if (menuItems[i].checked) {
       orderTotal += Number(menuItems[i].value);
     }
   }
-  console.log('checkbox clicked');
-  // Update the page with the formatted total
+
+  // Display the total
   document.getElementById('billTotal').innerHTML = formatCurrency(orderTotal);
-}
-
-let menuItems = document.getElementsByClassName('menuItem');
-
-for (let i = 0; i < menuItems.length; i++) {
-  menuItems[i].addEventListener('click', calcTotal);
 }
